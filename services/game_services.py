6f05d2ff2a409f59cc664539f9446c9954ed4281
[Marius Pruvot] <[marius.pruvot@outlook.fr]>
2023-11-25 22:42:36 +0100

    update docs with informative docstrings

--- services/game_services.py
+++ services/game_services.py
@@ -7,26 +7,54 @@ from services.player_services import *
 api_service = StrapiApiService()
 
 def create_game() -> Game:
-    """Create a new Game"""
+    """
+    Create a new Game instance and store it in the database.
+    Returns:
+        Game: The newly created Game instance.
+    """
     game = Game()
     return api_service.store_game_in_db(game)
 
 def retrieve_all_games() -> Optional[List[Game]]:
-    """Retrieve a list of all the games"""
+    """
+    Retrieve a list of all the games from the database.
+    Returns:
+        List[Game]: A list of Game instances.
+    Raises:
+        GameNotFoundError: If no games are found in the database.
+    """
     try:
         return api_service.get_games_from_db()
     except GameNotFoundError:
-        raise GameNotFoundError('List is empty!')
+        raise GameNotFoundError('List of games is empty!')
 
 def retrieve_single_game(game_uuid: str) -> Game:
-    """Retrieve a game by its UUID"""
+    """
+    Retrieve a game by its UUID from the database.
+    Args:
+        game_uuid (str): The UUID of the game to retrieve.
+    Returns:
+        Game: The Game instance with the specified UUID.
+    Raises:
+        GameNotFoundError: If no game with the specified UUID is found.
+    """
     try:
         return api_service.get_single_game(game_uuid)
     except GameNotFoundError:
         raise GameNotFoundError(f'No game found with UUID: {game_uuid}')
 
 def add_player_in_game(game_uuid: str, player_name: str) -> Game:
-    """Allows a Player to join an active game which is not already full"""
+    """
+    Allows a Player to join an active game which is not already full.
+    Args:
+        game_uuid (str): The UUID of the game to join.
+        player_name (str): The name of the player joining the game.
+    Returns:
+        Game: The updated Game instance after adding the player.
+    Raises:
+        PlayerNotFoundError: If the player with the specified name is not found.
+        GameNotFoundError: If the game with the specified UUID is not found.
+    """
     game = retrieve_single_game(game_uuid)
     player = get_single_player(player_name)
     updated_game = api_service.update_game_with_new_player(player, game)
