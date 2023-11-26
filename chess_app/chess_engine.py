fc5322a93a300daf1e2f62a36bca994e08ab1f47
[Marius Pruvot] <[marius.pruvot@outlook.fr]>
2023-11-26 16:20:14 +0100

    udpate chess engine with make move method

--- chess_app/chess_engine.py
+++ chess_app/chess_engine.py
@@ -13,14 +13,51 @@ class GameOfChess:
         self.player_2_color = 'Black' if self.player_1_color == 'White' else 'White'
         self.current_turn = self.player_1 if self.player_1_color == "White" else self.player_2
         
+    def init_board(self, fen : str):
+        """Initialize a ChessBoard from a Valid FEN
+        Args:
+            fen (str): The FEN of a chess Game
+        Raises:
+            VallueError: If the FEN is Not Valid
+        Returns:
+            Bool: True is FEN is Valid
+        """
+        try:
+            self.board.set_fen(fen)
+            return True
+        except ValueError as err:
+            raise ValueError(f"Please Enter a Valid FEN: error: {err}")
+
+        
     def get_player_turn(self):
         return self.current_turn
     
     def print_board(self):
         print(self.board)
 
-
+    def is_check(self):
+        return self.board.is_check()
+    
+    def end_of_game(self):
+        if self.board.is_checkmate():
+            return "Checkmate"
+        elif self.board.is_stalemate():
+            return "Stalemate"
+        elif self.board.is_insufficient_material():
+            return "Draw due to insufficient material"
+        return False
+    
+    def determine_winner(self):
+        if self.board.is_checkmate():
+            return self.player_2 if self.current_turn == self.player_1 else self.player_1
+        elif self.board.is_stalemate() or self.board.is_insufficient_material():
+            return "Draw"
+        
     def make_move(self, move: str, player_name: str):
+        if self.end_of_game():
+            winner = self.determine_winner()
+            raise GameOverError(f"Game Over. Winner: {winner}")
+        
         if player_name.capitalize() != self.current_turn.capitalize():
             raise InvalidTurnError(f'Not your turn, {self.current_turn} needs to play first.')
         try:
