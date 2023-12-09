1010c9edf83486906b86873ae33b37dd7667e2a8
[Marius Pruvot] <[marius.pruvot@outlook.fr]>
2023-12-09 14:34:01 +0100

    Implement Strapi API Service for Players and Games with Custom JSON Conversion

--- database_services/strapi_api_service.py
+++ database_services/strapi_api_service.py
@@ -1,5 +1,5 @@
 import json
-from typing import List
+from typing import List, Dict, Any
 import requests
 from custom_errors.custom_errors import (
     GameIsFullError,
@@ -8,18 +8,36 @@ from custom_errors.custom_errors import (
     PlayerAlreadyInGameError,
     PlayernotFoundError,
 )
-
 from schemas.chess_schemas import Game, Player
 
 
 class StrapiApiService:
     API_URL = "http://localhost:1337/api"
+    FILTER_PLAYER_BY_NAME = "/players?filters[name][$eq]="
 
     def __init__(self) -> None:
         """Initialize the Strapi API service."""
         pass
 
-    def get_players_from_db(self) -> List[Player]:
+    def _convert_json_to_player_object(self, response_data: Dict[str, Any]) -> Player:
+        """Converts player JSON data to a Player object."""
+        player_attributes = response_data["attributes"]
+        player_id = response_data["id"]
+        player = Player(**player_attributes)
+        player.player_id = player_id
+        return player
+
+    def _convert_json_to_game_object(self, response_data: Dict[str, Any]) -> Game:
+        """Converts game JSON data to a Game object."""
+        game_attributes = response_data["attributes"]
+        game_id = response_data["id"]
+        game = Game(**game_attributes)
+        game.game_id = game_id
+        return game
+
+    ## Get Methods
+
+    def get_players(self) -> List[Player]:
         """
         Retrieve a list of all players stored in the database.
 
@@ -27,148 +45,129 @@ class StrapiApiService:
             PlayernotFoundError: If no players are found in the database.
         """
         try:
-            r = requests.get(f"{self.API_URL}/players")
-            r.raise_for_status()
-            players_data = r.json()["data"]
-            return [Player(**player["attributes"]) for player in players_data]
+            response = requests.get(f"{self.API_URL}/players")
+            response.raise_for_status()
+            players_json = response.json()["data"]
+            if not players_json:
+                raise GameNotFoundError("No games found in the database.")
+            return [
+                self._convert_json_to_player_object(player) for player in players_json
+            ]
         except requests.exceptions.HTTPError:
             raise PlayernotFoundError("No players found in the database.")
 
-    def store_player_in_db(self, new_player: Player) -> Player:
+    def get_single_player(self, name: str) -> Player:
+        """
+        Retrieve a single player by name from the database.
+
+        Args:
+            name (str): The name of the player to retrieve.
+
+        Returns:
+            Player: An object representing the player's data if found.
+
+        Raises:
+            PlayernotFoundError: If no player with the specified name is found in the database.
+            requests.exceptions.HTTPError: If an HTTP error occurs during the API request.
+        """
+        try:
+            response = requests.get(f"{self.API_URL}{self.FILTER_PLAYER_BY_NAME}{name}")
+            response.raise_for_status()
+            player_json = response.json()["data"]
+            if not player_json:
+                raise PlayernotFoundError("No player with this name")
+            return self._convert_json_to_player_object(player_json[0])
+        except requests.exceptions.HTTPError as err:
+            raise err
+
+    def get_games(self) -> List[Game]:
+        """Retrieve a list of all games stored in the database."""
+        try:
+            response = requests.get(f"{self.API_URL}/games")
+            response.raise_for_status()
+            games_json = response.json()["data"]
+            if not games_json:
+                raise GameNotFoundError("No games found in the database.")
+            return [self._convert_json_to_game_object(game) for game in games_json]
+        except requests.exceptions.HTTPError as err:
+            raise err
+
+    def get_single_game(self, game_id: int) -> Game:
+        """
+        Retrieve a single game by its ID from the database.
+
+        Args:
+            game_id (int): The ID of the game to retrieve.
+
+        Returns:
+            Game: An object representing the game's data if found.
+
+        Raises:
+            GameNotFoundError: If no game with the specified ID is found in the database.
+            requests.exceptions.HTTPError: If an HTTP error occurs during the API request.
+        """
+        try:
+            response = requests.get(f"{self.API_URL}/games/{game_id}")
+            if response.status_code == 404:
+                raise GameNotFoundError(f"No game with ID {game_id} found")
+            response.raise_for_status()
+            game_json = response.json()["data"]
+            return self._convert_json_to_game_object(game_json)
+        except requests.exceptions.HTTPError as err:
+            raise err
+
+    ## Post Methods
+    
+    def post_players(self, new_player: Player) -> Player:
         """
         Store a new player instance in the database.
 
         Args:
             new_player (Player): The player instance to store.
 
+        Returns:
+            Player: The stored player object with updated data from the database.
+
         Raises:
             NameAlreadyExistsError: If a player with the same name already exists.
             HTTPError: If an HTTP error occurs during the request.
         """
-        if new_player.name.capitalize() in [
-            i.name.capitalize() for i in self.get_players_from_db()
-        ]:
-            raise NameAlreadyExistsError("A player with this name already exists !")
-
         try:
             new_player_json = json.loads(new_player.model_dump_json())
             payload = json.dumps({"data": new_player_json})
-            r = requests.post(
+            response = requests.post(
                 f"{self.API_URL}/players",
                 headers={"Content-Type": "application/json"},
                 data=payload,
             )
-            r.raise_for_status()
-            return Player(**r.json()["data"]["attributes"])
+            response.raise_for_status()
+            player_json = response.json()["data"]
+            return self._convert_json_to_player_object(player_json)
         except requests.exceptions.HTTPError as err:
-            raise requests.exceptions.HTTPError(
-                f"Failed to store player in database: {err}"
+            raise NameAlreadyExistsError(
+                f"A player with this Name already exists : {err}"
             )
 
-    def store_game_in_db(self, new_game: Game) -> Game:
-        """
-        Store a new game instance in the database.
-
-        Args:
-            new_game (Game): The game instance to store.
-
-        Raises:
-            HTTPError: If an HTTP error occurs during the request.
-        """
+    def post_games(self, new_game: Game) -> Game:
+        """Store a new game instance in the database."""
         try:
             new_game_json = json.loads(new_game.model_dump_json())
             payload = json.dumps({"data": new_game_json})
-            r = requests.post(
+            response = requests.post(
                 f"{self.API_URL}/games",
                 headers={"Content-Type": "application/json"},
                 data=payload,
             )
-            r.raise_for_status()
-            return Game(**r.json()["data"]["attributes"])
+            response.raise_for_status()
+            return self._convert_json_to_game_object(response.json()["data"])
         except requests.exceptions.HTTPError as err:
             raise requests.exceptions.HTTPError(
                 f"Failed to store game in database: {err}"
             )
 
-    def get_games_from_db(self) -> List[Game]:
-        """
-        Retrieve a list of all games stored in the database.
+    ## Put Methods
 
-        Raises:
-            GameNotFoundError: If no games are found in the database.
-        """
-        try:
-            r = requests.get(f"{self.API_URL}/games")
-            r.raise_for_status()
-            games_data = r.json()["data"]
-            if not games_data:
-                raise GameNotFoundError("No games found in the database.")
-            return [Game(**game["attributes"]) for game in games_data]
-
-        except requests.exceptions.HTTPError:
-            raise GameNotFoundError("No games found in the database.")
-
-    def get_single_game(self, game_uuid: str) -> Game:
-        """
-        Retrieve a single game by UUID and return a Game instance.
-        Args:
-            game_uuid (str): The UUID of the game to retrieve.
-        Raises:
-            GameNotFoundError: If the game with the given UUID is not found.
-        """
-        url = f"{self.API_URL}/games?filters[game_uuid][$eq]={game_uuid}"
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            games = response.json()
-            if not games["data"]:
-                raise GameNotFoundError(f"Game with UUID {game_uuid} not found.")
-            return Game(**games["data"][0]["attributes"])
-
-        except requests.exceptions.HTTPError as err:
-            raise GameNotFoundError(
-                f"Game with UUID {game_uuid} not found. HTTP error: {err}"
-            )
-
-    def get_strapi_game_id(self, game_uuid: str) -> int:
-        """
-        Retrieve the Strapi ID of a Game.
-        Args:
-            game_uuid (str): The UUID of the game.
-        Raises:
-            GameNotFoundError: If the game with the given UUID is not found.
-        """
-        url = f"{self.API_URL}/games?filters[game_uuid][$eq]={game_uuid}"
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            id = response.json()["data"][0].get("id")
-            return id
-        except requests.exceptions.HTTPError as err:
-            raise GameNotFoundError(
-                f"Game with UUID {game_uuid} not found. HTTP error: {err}"
-            )
-
-    def get_strapi_player_id(self, player_name: str):
-        """
-        Retrieve the Strapi ID of a Player.
-        Args:
-            player_name (str): The UUID of the Player.
-        Raises:
-            PlayerNotFoundError: If the Player with the given namm is not found.
-        """
-        url = f"{self.API_URL}/players?filters[name][$eq]={player_name.capitalize()}"
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            print(response.json())
-            id = response.json()["data"][0].get("id")
-            return id
-        except requests.exceptions.HTTPError as err:
-            raise PlayernotFoundError(f"Player with name {player_name} not found !")
-
-
-    def update_player_active_games(self, player: Player, game : Game = None) -> Player:
+    def update_player_active_games(self, player: Player, game: Game = None) -> Player:
         if game in player.active_games:
             raise PlayerAlreadyInGameError(
                 f"Player {player.name} with uuid : {player.player_uuid} already join this game ! "
@@ -177,12 +176,10 @@ class StrapiApiService:
         url = f"{self.API_URL}/players/{player_id}"
         if game:
             player.active_games.append(game.game_uuid)
-            
+
         player_data_json = json.loads(player.model_dump_json())
         payload = json.dumps({"data": player_data_json})
-    
 
-        
         try:
             response = requests.put(
                 url, headers={"Content-Type": "application/json"}, data=payload
@@ -192,7 +189,7 @@ class StrapiApiService:
             return Player(**updated_player_data)
         except requests.exceptions.HTTPError as err:
             raise requests.exceptions.HTTPError(f"HTTP error occurred: {err}")
-    
+
     def update_game_with_new_player(self, player: Player, game: Game) -> Game:
         """
         Update a game with a new player.
@@ -216,7 +213,7 @@ class StrapiApiService:
 
         game.players.append(updated_player)
         game_data_json = json.loads(game.model_dump_json())
-    
+
         payload = json.dumps({"data": game_data_json})
 
         try:
